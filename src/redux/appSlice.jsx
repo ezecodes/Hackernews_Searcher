095f241cbceea5eb929016b5d2dd14408de69a71
byt-32 <null@gmail.com>
2022-05-19 14:03:59 -0800

    add extraReducers object to handle fetchPosts life cycle

--- src/redux/appSlice.jsx
+++ src/redux/appSlice.jsx
@@ -1,16 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+export const fetchPosts = createAsyncThunk(
+	'posts',
+	async (query) => {
+		const response = await fetch(query)
+		return response.json()
+	}
+)
 
 const initialState = {
-
+	results: [],
+	loader: false
 }
 
 const appSlice = createSlice({
 	name: 'app',
 	initialState,
-	reducers: {
-
+	extraReducers: (builder) => {
+		builder.addCase(fetchPosts.pending, (state, action) => {
+			state.loader = true
+		})
+		builder.addCase(fetchPosts.fulfilled, (state, action) => {
+			state.results = [...action.payload.hits]
+			state.loader = false
+		})
 	}
 })
 
-// export {} = appSlice.actions
+export const {getResults} = appSlice.actions
 export default appSlice.reducer
