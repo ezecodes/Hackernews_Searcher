75a80c3f93f5aed227f3d356d34e74130e259ef8
byt-32 <null@gmail.com>
2022-05-19 13:58:42 -0800

    create new folder for SearchResults and Post components

---
+++ src/main/Body/searchResults/SearchResults.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { useSelector } from 'react-redux'
+import Post from './Post'
+import { Preloader, TailSpin } from 'react-preloader-icon'
+
+const useStyles = makeStyles({
+	resultsMain: {
+		width: '100%'
+	},
+	loader: {
+		display:'flex',
+		justifyContent: 'center',
+		marginTop: '30px'
+	},
+	results: {
+
+	},
+})
+
+const SearchResults = () => {
+	const classes = useStyles()
+	const hits = useSelector(state => state.app.results)
+	const loader = useSelector(state => state.app.loader)
+	return (
+		<section className={classes.resultsMain} >
+			{ loader ?<div className={classes.loader}>
+				<Preloader 
+			    use={TailSpin}
+			    size={50}
+			    strokeWidth={6}
+			    strokeColor="#262626"
+			    duration={2000}
+			  />
+			</div>
+			: <div className={classes.results}>
+				{
+					hits.map((post, i) => {
+						return (
+							<Post key={i} post={post} />
+						)
+					})
+				}
+			</div>}
+		</section>
+	)
+}
+
+export default SearchResults
