ec156ed810e7501188cc60b76e64f4849128cb13
byt-32 <null@gmail.com>
2022-05-20 01:42:38 -0800

    define postDetail component

--- src/main/postDetails/PostDetails.jsx
+++ src/main/postDetails/PostDetails.jsx
@@ -1,16 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { makeStyles }from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import CommentIcon from '@material-ui/icons/Comment';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import UserAvatar from './UserAvatar';
-import Typography from '@material-ui/core/Typography';
+import PostDetail from './PostDetail'
 
 const useStyles = makeStyles({
 	postDetails: {
@@ -31,77 +22,18 @@ const useStyles = makeStyles({
 		// 	padding: '0'
 		// }
 	},
-	card: {
-		width: '100%',
-		'&& .MuiCardHeader-title': {
-			fontWeight: 'bold'
-		},
-		'&& .MuiCardHeader-subheader': {
-		}
-	},
-	cardContent: {
-		padding: '3px 16px',
-		'&& .MuiTypography-body1': {
-			fontSize: '1rem',
-			fontWeight: 'bold'
-		},
-		'&& .MuiTypography-body2': {
-			color: 'rgb(0 0 0 / 99%)',
-			fontSize: '.965rem',
-			lineHeight: '1.4'
-		}
-	},
-	avatar: {
-		textTransform: 'uppercase',
-	},
+	
+	
 })
-
-// const parseHTML = (text) => {
-// 	const parser = new DOMParser()
-// 	return parser.parseFromString(story.text, 'text/html')
-// }
-
-const getDate = (date) => {
-	return (/[0-9]*-[0-9]*-[0-9]*/).exec(date)[0]
-}
 
 const PostDetails = () => {
 	const classes = useStyles()
 	const story = useSelector(state => state.app.postDetails)
-	console.log(story)
+	const loader = useSelector(state => state.app.loader.postDetails)
 	return (
 		<section className={classes.postDetails} >
 			<div className={classes.detailsContent}>
-				<Card className={classes.card} >
-					<CardHeader 
-						avatar={
-							<UserAvatar name={story.author}
-								className={classes.avatar} 
-							/> 
-						}
-						title={story.author}
-						subheader={getDate(story.created_at)}
-					/>
-					<CardContent className={classes.cardContent} >
-						<Typography variant='body1' component='h1'>
-							{story.title}
-						</Typography>
-						 <Typography variant="body2" color="textSecondary" component="p" 
-						 	dangerouslySetInnerHTML={{__html: story.text }} />
-					</CardContent>
-					<CardActions disableSpacing>
-						<span className={classes.actions} >
-							<ArrowDropUpIcon />
-							<span className={classes.actionInfo}>{story.points}</span>
-						</span>
-						<span className={classes.actions} >
-							<IconButton>
-								<CommentIcon />
-							</IconButton>
-							<span className={classes.actionInfo}>{story.children.length}</span>
-						</span>
-					</CardActions>
-				</Card>
+				{!loader && <PostDetail story={story} />}
 			</div>
 		</section>
 	)
