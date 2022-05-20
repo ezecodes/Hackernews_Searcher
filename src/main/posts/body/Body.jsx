017a32a2f0029840d6d6ed5eb4a5ebcd950ede52
byt-32 <null@gmail.com>
2022-05-20 15:25:56 -0800

    refactor

--- src/main/posts/body/Body.jsx
+++ src/main/posts/body/Body.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import SearchResults from './searchResults/SearchResults'
 import { makeStyles } from '@material-ui/core/styles'
+import IconButton from '@material-ui/core/IconButton'
+import EditAttributesIcon from '@material-ui/icons/EditAttributes'
 
 const useStyles = makeStyles({
 	body: {
 		width: '100%',
 		display: 'flex',
-		padding: '0 17px',
-		margin: '16px 0'
-	}
+		margin: '21px 0 0 0',
+		padding: '0 59px',
+	},
+	bodyContent: {
+		width: '100%',
+		
+	},
+	
 })
 
 const Body = () => {
 	const classes = useStyles()
 	return (
 		<section className={classes.body}>
-			<SearchResults />
+			<div className={classes.bodyContent}>
+				<SearchResults />
+			</div>
 		</section>
 	)
 }
