8b08759bbc7085ac79ffabe1580ef49c335b257b
byt-32 <null@gmail.com>
2022-06-18 03:48:56 -0800

    make headers show/hide on scroll

--- src/main/posts/Posts.jsx
+++ src/main/posts/Posts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Header from './header/Header'
+import ScrollSpyHeader from './header/ScrollSpyHeader'
 import Body from './body/Body'
 
 
@@ -28,10 +29,31 @@ const useStyles = makeStyles({
 })
 const Posts = () => {
 	const classes = useStyles()
+	const ref = React.createRef()
+	const [show, setDisplay]= React.useState({
+		mainHeader: true, scrollHeader: false
+	})
+
+	const handleHeaders = () => {
+		if (ref.current !== null) {
+			const height = ref.current.getBoundingClientRect().height
+			const winY = window.scrollY
+			if (winY === 0) {
+				setDisplay({...show, mainHeader: true, scrollHeader: false})
+			}
+			if (height < winY) {
+				setDisplay({...show, mainHeader: false, scrollHeader: true})
+			} else {
+				setDisplay({...show, mainHeader: true, scrollHeader: false})
+			}
+		}
+	}
+	window.addEventListener('scroll', handleHeaders)
 	return (
 		<section className={classes.posts}>
 			<section className={classes.content}>	
-				<Header />
+				<Header ref={ref} inlineStyles={{display: !show.mainHeader && 'none'}} />
+				<ScrollSpyHeader inlineStyles={{display: !show.scrollHeader && 'none'}}  />
 				<Body />
 			</section>
 			
